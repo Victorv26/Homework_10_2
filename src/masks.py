c417310199a736e5b3e62883b22d3e87edef0b07
Viktor Kozhukharov <kozhukharov@mail.ru>
2024-09-26 18:57:56 +0300

    end

--- src/masks.py
+++ src/masks.py
@@ -7,7 +7,7 @@ def get_mask_card_number(card_number: Union[int, str]) -> Union[int, str]:
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number())
+    print(get_mask_card_number(1596837868705199))
 
 
 def get_mask_account(count_number: Union[int, str]) -> Union[int, str]:
@@ -16,4 +16,4 @@ def get_mask_account(count_number: Union[int, str]) -> Union[int, str]:
 
 
 if __name__ == "__main__":
-    print(get_mask_account())
+    print(get_mask_account(64686473678894779589))
--- src/widget.py
+++ src/widget.py
@@ -2,23 +2,26 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(type_and_number: str) -> str:
-    '''Функция маскирующая счет/карту'''
-    text_result = ''
+    """Функция маскирующая счет/карту"""
+    text_result = ""
+    digit_result = ""
     digit_count = 0
     for symbol in type_and_number:
         if symbol.isalpha():
             text_result += symbol
         elif symbol.isdigit():
             digit_count += 1
-    if digit_count > 16:
-        return text_result + str(get_mask_account)
-    else:
-        return text_result + str(get_mask_card_number)
+            digit_result += symbol
+    if digit_count == 20:
+        return f"{text_result} {get_mask_account(digit_result)}"
+    elif digit_count == 16:
+        return f"{text_result} {get_mask_card_number(digit_result)}"
 
-#print(mask_account_card('Счет 35383033474447895560'))
+
+print(mask_account_card("Maestro 1596837868705199"))
 
 
 def get_date(date_str: str) -> str:
-    '''Функция принимает строку и возвращает дату в формате ДД.ММ.ГГГГ'''
-    date_slice = date_str[0:10].split('-')
-    return '.'.join(date_slice[::-1])
+    """Функция принимает строку и возвращает дату в формате ДД.ММ.ГГГГ"""
+    date_slice = date_str[0:10].split("-")
+    return ".".join(date_slice[::-1])
