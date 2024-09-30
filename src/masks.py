3be3547f7221008cab9ba280f09c0730ab868035
Viktor Kozhukharov <kozhukharov@mail.ru>
2024-09-30 19:11:59 +0300

    add processin.py

--- src/masks.py
+++ src/masks.py
@@ -6,14 +6,7 @@ def get_mask_card_number(card_number: Union[int, str]) -> Union[int, str]:
     return f"{str(card_number)[0:4]} {str(card_number)[4:6]}** **** {str(card_number)[12:]}"
 
 
-if __name__ == "__main__":
-    print(get_mask_card_number(1596837868705199))
-
-
 def get_mask_account(count_number: Union[int, str]) -> Union[int, str]:
     """Функция маскировки номера банковского счета"""
     return f"**{str(count_number)[-4:]}"
 
-
-if __name__ == "__main__":
-    print(get_mask_account(64686473678894779589))
