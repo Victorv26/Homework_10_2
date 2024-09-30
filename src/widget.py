105dafd12b6f15cafb3b2c3500d5b3a7cd5ed275
Viktor Kozhukharov <kozhukharov@mail.ru>
2024-09-30 17:52:22 +0300

    add new version of function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -3,19 +3,11 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(type_and_number: str) -> str:
     """Функция маскирующая счет/карту"""
-    text_result = ""
-    digit_result = ""
-    digit_count = 0
-    for symbol in type_and_number:
-        if symbol.isalpha():
-            text_result += symbol
-        elif symbol.isdigit():
-            digit_count += 1
-            digit_result += symbol
-    if digit_count == 20:
-        return f"{text_result} {get_mask_account(digit_result)}"
-    elif digit_count == 16:
-        return f"{text_result} {get_mask_card_number(digit_result)}"
+    new_number = type_and_number.split()
+    if len(new_number[-1]) == 20:
+        return f"{"".join(new_number[:-1])} {get_mask_account(new_number[-1])}"
+    elif len(new_number[-1]) == 16:
+        return f"{"".join(new_number[:-1])} {get_mask_card_number(new_number[-1])}"
 
 
 def get_date(date_str: str) -> str:
